7eef145214f8e5529f2cf72bfa84cb29c46f4324
Poojanavgurukul <Pooja17@navgurukul.org>
2018-09-24 20:17:52 +0530

    adding login and signup components

---
+++ src/Components/Homepage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './Navbar.js';
+import NormalLoginForm from './Loginpage.js';
+import Signup from './Signup.js';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+
+class Homepage extends React.Component{
+    render(){
+        return(
+            <Router>
+                <div>
+                    <Route exact path ='/' component ={Navbar}/>
+                   <Route path ='/login' component={NormalLoginForm} />
+                   <Route path='/signup' component= {Signup} />
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default Homepage;
